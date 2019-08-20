e804dd6edda113d9ff55705cdbac889bf6ded2ea
sksamuel <sam@sksamuel.com>
2019-08-20 18:33:02 -0500

    wip

--- kotlintest-plugin.js
+++ kotlintest-plugin.js
@@ -1,18 +1,21 @@
 var kotlin_test = require('./build/node_modules/kotlin-test.js');
 
+console.log("weeee");
+
 kotlin_test.setAdapter({
     suite: function (name, ignored, fn) {
-        if (name == "FunSpec" ||
-            name == "BehaviorSpec" ||
-            name == "StringSpec")
-            return
-        describe(name, fn)
+        console.log("hellooo " + name);
+        // if (name == "FunSpec" ||
+        //     name == "BehaviorSpec" ||
+        //     name == "SuiteSpec" ||
+        //     name == "StringSpec")
+        //     return;
+       // QUnit.module(name, fn);
     },
     test: function (name, ignored, fn) {
-        if (name == "kotlintest_generate_tests") {
-           fn()
-        } else {
-           it(name, fn)
-        }
+        console.log("HELLLLLLLO " + name);
+        // if (name === "kotlintestGenerateTests3") {
+        //    fn()
+        // }
     }
 });
--- src/commonTest/kotlin/com/sksamuel/kotlineverywhere/ValidationTest.kt
+++ src/commonTest/kotlin/com/sksamuel/kotlineverywhere/ValidationTest.kt
@@ -1,22 +1,37 @@
-package com.sksamuel.kotlineverywhere
-
-import io.kotlintest.core.SuiteSpec
-import io.kotlintest.shouldBe
-
-class ValidationTest : SuiteSpec({
-  suite("social regex") {
-    test("should pass valid numbers") {
-      validateSocial("123-456-1111") shouldBe true
-      validateSocial("999-333-4444") shouldBe true
-    }
-    test("should fail invalid numbers") {
-      validateSocial("a12-456-1111") shouldBe false
-      validateSocial("a12-456-cccc") shouldBe false
-      validateSocial("123456-1111") shouldBe false
-      validateSocial("123-4561117") shouldBe false
-      validateSocial("122") shouldBe false
-      validateSocial("") shouldBe false
-      validateSocial("1234567899") shouldBe false
-    }
-  }
-})
+//package com.sksamuel.kotlineverywhere
+//
+//import io.kotlintest.core.SuiteSpec
+//import io.kotlintest.shouldBe
+//
+//class ValidationTest : SuiteSpec({
+//
+//  suite("social regex") {
+//    test("should pass valid numbers") {
+//      validateSocial("123-456-1111") shouldBe true
+//      validateSocial("999-333-4444") shouldBe true
+//    }
+//    test("should fail invalid numbers") {
+//      validateSocial("a12-456-1111") shouldBe false
+//      validateSocial("a12-456-cccc") shouldBe false
+//      validateSocial("123456-1111") shouldBe false
+//      validateSocial("123-4561117") shouldBe false
+//      validateSocial("122") shouldBe false
+//      validateSocial("") shouldBe false
+//      validateSocial("1234567899") shouldBe false
+//    }
+//  }
+//
+//  suite("email regex") {
+//    test("should require an @ and tld") {
+//      validateSocial("a@a.com") shouldBe true
+//      validateSocial("sam@sam.com") shouldBe true
+//    }
+//    test("should fail without tld") {
+//      validateSocial("a@a") shouldBe false
+//      validateSocial("@a") shouldBe false
+//    }
+//    test("should fail without an @") {
+//      validateSocial("a.com") shouldBe false
+//    }
+//  }
+//})
